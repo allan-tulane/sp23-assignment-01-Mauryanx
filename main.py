aae4709ef571b535f15d5b834a6001b02881a2e4
Mauryanx <115839207+Mauryanx@users.noreply.github.com>
2023-02-14 21:34:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ See assignment-01.pdf for details.
 # no imports needed.
 
 def foo(x):
-    ### TODO
-    pass
+    if x <= 1:
+        return x
+    else:
+        val = foo(x - 1) + foo(x - 2)
+        return val
 
 def longest_run(mylist, key):
     ### TODO
